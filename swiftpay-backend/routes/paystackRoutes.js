00a7212617f6c0abc522004da144a0edcd41c548
Samuel Tomisin <samueltomisin06@outlook.com>
2025-08-02 15:04:41 +0100

    Update db

--- swiftpay-backend/routes/paystackRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authMiddleware = require("../middleware/authMiddleware");
-const paystackController = require('../controllers/paystackController');
-
-
-router.post("/initialize", authMiddleware, paystackController.initializePayment);
-
-module.exports = router;
--- swiftpay-backend/routes/transactionRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getTransactionsByUser } = require('../controllers/transactionController');
-
-router.get('/:userId', getTransactionsByUser);
-
-module.exports = router;
